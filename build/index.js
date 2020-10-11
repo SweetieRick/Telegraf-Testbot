b5b3de95c2f9c630fde8234abc277b027098311e
SuperRichyMario <65025304+SweetieRick@users.noreply.github.com>
2020-10-11 19:23:20 +0200

    Initial steps

--- build/index.js
+++ build/index.js
@@ -3,7 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var chalk_1 = __importDefault(require("chalk"));
 var telegraf_1 = __importDefault(require("telegraf"));
-var token = process.env.TOKEN;
 var TeleBot = new telegraf_1.default('1304224773:AAF1d47HtH9oWgPBYi_ggu1VTtN1jUkpliQ');
-TeleBot.start(function (ctx) { return ctx.reply('Hey there, welcome to Ricks Playground!'); });
+// ? Simple bot functions
+TeleBot.start(function (ctx) { return ctx.reply('Hey there, welcome to Ricks Playground! Do /help for help!'); });
+TeleBot.help(function (ctx) { return ctx.reply('I can do many things, but all my commands are: \n /communism ~ Only 4 true slav man'); });
+TeleBot.on('sticker', function (ctx) { return ctx.reply('👍'); });
+TeleBot.command('communism', function (ctx) { return ctx.reply('All hail stalin!'); });
+// ? Launching bot application
+console.log(chalk_1.default.redBright('APP LAUNCHED'), 'Application was launched');
+TeleBot.launch();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+import chalk from "chalk";
 import Telegraf from "telegraf";
-const token = process.env.TOKEN
+const token = ''
 
-const TeleBot = new Telegraf('1304224773:AAF1d47HtH9oWgPBYi_ggu1VTtN1jUkpliQ')
-TeleBot.start((ctx) => ctx.reply('Hey there, welcome to Ricks Playground!'))
+const TeleBot = new Telegraf(token)
+
+// ? Simple bot functions
+TeleBot.start((ctx) => ctx.reply('Hey there, welcome to Ricks Playground! Do /help for help!'))
+TeleBot.help((ctx) => ctx.reply('I can do many things, but all my commands are: \n /communism ~ Only 4 true slav man'))
+TeleBot.on('sticker', (ctx) => ctx.reply('👍'))
+TeleBot.command('communism', (ctx) => ctx.reply('All hail stalin!'))
+
+// ? Launching bot application
+console.log(chalk.redBright('APP LAUNCHED'), 'Application was launched')
+TeleBot.launch()
